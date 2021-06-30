02fcb07d2b2e30f8fe627cdcb68512859d32e042
PoWeiChiao <pwchiao@hotmail.com>
2021-06-30 20:59:44 +0800

    update

---
+++ SmartPointer/weakptr.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+// #include <memory>
+
+using namespace std;
+
+// class A {
+//     A() : a(1) { cout << "A Constructor..." << endl; }
+//     ~A() { cout << "A Destructor..." << endl; }
+//     int a;
+// };
+
+int main() {
+    // shared_ptr<A> a(new A()));
+    // weak_ptr<A> wp = a;
+    // cout << a.use_count() << endl;
+    return 0;
+}
